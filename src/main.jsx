189ff6142350089d20ae623760824ee8205568ca
Natty24 <malifeaye2485@gmail.com>
2026-04-06 01:45:16 +0300

    fix: downgrade i18next versions for compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './i18n' // 👈 ይህ መስመር ከሌለ ቋንቋው አይሰራም!
+import './i18n' // 👈 ይህ መስመር መኖሩን 100% አረጋግጥ!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
